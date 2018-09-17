bf3327f96292535f23bb33b2a7b9fe1005a353c6
TWalk007 <walkertb03@gmail.com>
2018-09-17 08:53:32 -0400

    Update to version 0.15
    Also new version of Unity 2018.2.8f1

---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+    public float resetTime = 4.0f;
+
+	private SteamVR_TrackedObject trackedObj;
+	private GameObject collidingObject;
+	private GameObject objectInHand;
+
+	private bool objectInHandCheck = false;
+
+	private SteamVR_Controller.Device Controller
+	{
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	private void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	private void SetCollidingObject(Collider col)
+	{
+		if (collidingObject || !col.GetComponent<Rigidbody>())
+		{
+			return;
+		}
+		collidingObject = col.gameObject;
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		SetCollidingObject(other);
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		SetCollidingObject(other);
+    }
+
+	public void OnTriggerExit(Collider other)
+	{
+		if (!collidingObject)
+		{
+			return;
+		}
+		collidingObject = null;
+	}
+
+	private void GrabObject()
+	{
+		objectInHand = collidingObject;
+		collidingObject = null;
+
+		var joint = AddFixedJoint();
+		joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+		objectInHandCheck = true;
+		if (objectInHand.GetComponent<HighlightController>())
+		{
+			objectInHand.GetComponent<HighlightController>().objectInHand = objectInHandCheck;
+		}
+	}
+
+	private FixedJoint AddFixedJoint()
+	{
+		FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+		fx.breakForce = 20000;
+		fx.breakTorque = 20000;
+		return fx;
+	}
+
+	private void ReleaseObject()
+	{
+		if (GetComponent<FixedJoint>())
+		{
+			GetComponent<FixedJoint>().connectedBody = null;
+			Destroy(GetComponent<FixedJoint>());
+
+			objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+			objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+		}
+		objectInHandCheck = false;
+		if (objectInHand.GetComponent<HighlightController>())
+		{
+			objectInHand.GetComponent<HighlightController>().objectInHand = objectInHandCheck;
+		}
+
+		objectInHand = null;
+	}
+
+	void Update () {
+		if (Controller.GetHairTriggerDown())
+		{
+			if (collidingObject)
+			{
+				GrabObject();
+			}           
+		}
+
+		if (Controller.GetHairTriggerUp())
+		{
+			if (objectInHand)
+			{
+				ReleaseObject();
+			}
+		}		      
+	}
+}
